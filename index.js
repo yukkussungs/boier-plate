a71966d9eb40dc7299afa9d8533d409f01dbadaa
woosung.shin <sws7420@gmail.com>
2020-12-30 00:51:47 +0900

    featuren create login and logout api

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
-const { User } = require('./models/Users');
-
+const { auth } = require('./middleware/auth');
+const { User } = require('./models/User');
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 // application/x-www-form-urlencoded
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // application/josn
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 
 const mongoose = require('mongoose')
@@ -24,7 +26,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!~~ 안녕하세요~~')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
     // 회원가입 할때 필요한 정보들을 client에서 가져오면
     //  그것들을 데이터 베이스에 넣어준다.
@@ -41,6 +43,56 @@ app.post('/register', (req, res) => {
         })
     })
 
+})
+
+app.post('/api/users/login', (req, res) => {
+    //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+    User.findOne({ email: req.body.email },(err, userInfo) => {
+        if(!userInfo) {
+            return res.json({
+                loginSuccess: false,
+                message: '제공된 이메일에 해당하는 유저가 없습니다.'
+            })
+        }
+    })
+
+    //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+        if (!isMatch) return res.json({ loginSuccess: false, message:"비밀번호가 틀렸습니다." });
+
+        //비밀번호까지 맞다면 토큰을 생성하기
+        user.generateToken((err, user) => {
+           if (err) return res.status(400).send(err);
+           
+           // 토큰을 저장한다. 어디에 ? 쿠키 or 로컬스토리지
+            res.cookie("x_auth", user.token) 
+            .status(200)
+            .json({ loginSuccess: true, userId: user._id })
+
+        })
+    })
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+    res.status(200).json({
+        _id: req.user._id,
+        idAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.role,
+        image: req.user.image
+    })
+})
+
+app.get('/api/users/logout', auth, (res, req) => {
+    User.findOneAndUpdate({ _id: req.user._id }, 
+        {token: ""}, (err, user) => {
+            if (err) return res.json({ success: false, err });
+            return res.status(200).send({
+                success: true
+            })
+        })
 })
 
 app.listen(port, () => {
